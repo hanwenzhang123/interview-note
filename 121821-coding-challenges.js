72f4a2af2e5d67017be2aaa4cc200ba2500d6c6e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-12-18 20:32:36 -0500

    Update 121821-coding-challenges.js

--- 121821-coding-challenges.js
+++ 121821-coding-challenges.js
@@ -42,8 +42,96 @@ getLmit must return the maximum number that our queue can hold.
 This must be done in Java if possible.
 ```
 
-//loom coding challenges
 
+//loom coding challenges
+- What is a RESTful API and what are its advantages?
+  RESTful API provides a set of tools that allows us to build apps via HTTP requests through the CRUD operations.
+
+1.
+```
+String Challenge
+Have the function StringChallenge(str) take the str parameter being passed which will be an average rating between 0.00 and 5.00, 
+and convert this rating into a list of 5 image names to be displayed in a user interface to represent the rating as a list of stars and half stars. 
+Ratings should be rounded to the nearest half. There are 3 image file names available: "full.jpg", "half.jpg", "empty.jpg". 
+The output will be the name of the 5 images (without the extension), from left to right, separated by spaces. 
+For example: if str is "2.36" then this should be displayed by the following image:
+
+Input: "0.38"
+Output: half empty empty empty empty
+Input: "4.5"
+Output: full full full full half
+```
+function StringChallenge (str) {
+  const rating = Number(str);
+  const remainder = rating % 1;
+  let completeStars = Math.floor(rating - remainder);
+  let hasFractionalStar = remainder >= .25 || remainder < .75;
+
+  const result = [];
+  for (let i = 0; i < 5; i++) {
+    if (completeStars) {
+      result.push('full');
+      completeStars -= 1;
+    }
+    else if (hasFractionalStar) {
+      result.push('half');
+      hasFractionalStar = false;
+    } else {
+      result.push('empty');
+    }
+  }
+
+  return result.join(' ');
+}
+   
+// keep this function call here 
+console.log(StringChallenge(readline()));
+
+//2
+```
+For this challenge you will determine whether HTML elements are nested correctly.
+Have the function StringChallenge(str) read the str parameter being passed which will be a string of HTML DOM elements and plain text. 
+The elements that will be used are: b, i, em, div, p. 
+For example: if str is "<div><b><p>hello world</p></b></div>" then this string of DOM elements is nested correctly so your program should return the string true.
+If a string is not nested correctly, return the first element encountered where, 
+if changed into a different element, would result in a properly formatted string. 
+If the string is not formatted properly, then it will only be one element that needs to be changed. 
+For example: if str is "<div><i>hello</i>world</b>" then your program should return the string div 
+because if the first <div> element were changed into a <b>, the string would be properly formatted.
+
+Input: "<div><div><b></b></div></p>"
+Output: div
+Input: "<div>abc</div><p><em><i>test test test</b></em></p>"
+Output: i
+```
+function StringChallenge(str) { 
+  let a = str.split("<").join("");
+  let b = a.split(">");
+  let result;
+
+  for (let i = 0; i <= b.length; i ++) {
+    let temp;
+
+    if(b[i]){
+        if(b[i].includes("/")){
+          let counter = 1;
+          temp = b[i - counter];
+          if(b[i].includes(temp)){
+            counter++;
+            result = true;
+          } else if (!b[i].includes(temp)) {
+            counter++;
+            result = temp;
+            return result
+          }
+        }
+      }
+    }
+  return result; 
+}
+   
+// keep this function call here 
+console.log(StringChallenge(readline()));
 
 
 //zippity coding challenges
