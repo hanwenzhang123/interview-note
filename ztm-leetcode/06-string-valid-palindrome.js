c4d813a51be2d441b591a685cb59e834130f5fed
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-12-22 11:36:11 -0500

    Update 06-string-valid-palindrome.js

--- ztm-leetcode/06-string-valid-palindrome.js
+++ ztm-leetcode/06-string-valid-palindrome.js
@@ -1,1 +1,9 @@
+sub-problems
+- a sunproblem is a problem we have to solve along the way to solving the main problem
 
+main problem:
+sub problem:
+
+
+A palindrome is a string that reads the same forwards and backwards
+"aba" "a" "race car" "" "A man, a plan, a canal: Panama"
