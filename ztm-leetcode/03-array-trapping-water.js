d735d7feedb8743dc1fb41e984f0f2bd6ebb46c4
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-12-26 15:01:51 -0500

    Update 03-array-trapping-water.js

--- ztm-leetcode/03-array-trapping-water.js
+++ ztm-leetcode/03-array-trapping-water.js
@@ -1,1 +1,92 @@
 //Question #3: Tapping Rainwater (Hard)
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+step 1: verify the constraints
+
+step 2: write out some test cases
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+step 3: figure out a solution without code
+step 4: write out our solution in code
+//trapping rainwater - brute force solution
+const elevationArray = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
+
+const getTrappedRainwater = function(heights) {
+  let totalWater = 0;
+  
+  for(let p = 0; p < heights.length; p++) {
+    let leftP = p, rightP = p, maxLeft = 0, maxRight = 0;
+
+    while(leftP >= 0) {
+      maxLeft = Math.max(maxLeft, heights[leftP]);
+      leftP--;
+    }
+
+    while(rightP < heights.length) {
+      maxRight = Math.max(maxRight, heights[rightP]);
+      rightP++;
+    }
+    
+    const currentWater = Math.min(maxLeft, maxRight) - heights[p];
+    
+    if(currentWater >= 0) {
+      totalWater += currentWater;
+    }
+  }
+
+  return totalWater;
+}
+
+console.log(getTrappedRainwater(elevationArray));
+
+step 5: double check for errors
+step 6: test our code with our test cases
+step 7: space & time complexity
+step 8: can we optimize our solution?
+  //trapping rainwater - optimal solution
+const elevationArray = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
+
+/*
+1. Identify the pointer with the lesser value
+2. Is this pointer value greater than or equal to max on that side
+  yes -> update max on that side
+  no -> get water for pointer, add to total
+3. move pointer inwards
+4. repeat for other pointer
+ */
+
+const getTrappedRainwater = function(heights) {
+
+  let left = 0, right = heights.length - 1, totalWater = 0, maxLeft = 0, maxRight = 0;
+  
+  while(left < right) {
+    if(heights[left] <= heights[right]) {
+      if(heights[left] >= maxLeft) { 
+        maxLeft = heights[left]
+      } else { 
+        totalWater += maxLeft - heights[left];
+      }
+      left++;
+    } else {
+      if(heights[right] >= maxRight) {
+          maxRight = heights[right];
+      } else {
+          totalWater += maxRight - heights[right];
+      }
+        
+      right--;
+    }
+  }
+
+  return totalWater;
+}
+
+
+console.log(getTrappedRainwater(elevationArray));
+  
+  
