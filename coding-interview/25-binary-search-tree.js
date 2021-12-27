3eef31f25996d729f35b984a25fc37aaa7b26578
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-12-26 21:15:27 -0500

    Update 25-binary-search-tree.js

--- coding-interview/25-binary-search-tree.js
+++ coding-interview/25-binary-search-tree.js
@@ -6,6 +6,23 @@ value > left.value  value < right.value
 binary tree - normal tree that just has two nodes underneath each parent, every node in here has at most two children.
 binary search tree - the values on the left are less than the parent value and that the values on the right are greater than the parent value.
 
+//Binary Search Implementation
+const binarySearch = function(array, target){
+  let left = 0, right = array.length - 1;
+  while(left <= right){
+    const mid = Math.floor((left + right) / 2);
+    const foundValue = array[mid];
+    if(foundValue === target){
+      return mid;
+    } else if(foundValue < target){
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
 //Binary Search Tree - Implementation
 
 // --- Directions
