8a98f3d1d0a180d515ed583fdebdd828cbb92260
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-12-30 14:38:27 -0500

    Create 22-2d-array-walls-and-gates.js

---
+++ ztm-leetcode/22-2d-array-walls-and-gates.js
@@ -0,0 +1,42 @@
+
+
+const INF = 2147483647;
+
+const testMatrix = [
+  [INF, -1, 0, INF],
+  [INF, INF, INF, 0],
+  [INF, -1, INF, -1],
+  [0, -1, INF, INF]
+];
+
+const WALL = -1;
+const GATE = 0;
+const EMPTY = 2147483647;
+const directions = [
+  [-1, 0], //up
+  [0, 1], //right
+  [1, 0], //down
+  [0, -1] //left
+]
+
+const dfs = (grid, row, col, count) => {
+  if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || count > grid[row][col]) return
+  grid[row][col] = count
+  for(let i = 0; i < directions.length; i++) {
+    const currentDir = directions[i];
+    dfs(grid, row + currentDir[0], col + currentDir[1], count + 1);
+  }
+}
+
+
+const wallsAndGates = (rooms) => {
+  for (let row = 0; row < rooms.length; row++) {
+    for (let col = 0; col < rooms[0].length; col++) {
+      if (rooms[row][col] === GATE) dfs(rooms, row, col, 0)
+    }
+  }
+};
+
+wallsAndGates(testMatrix)
+
+console.log(testMatrix);
