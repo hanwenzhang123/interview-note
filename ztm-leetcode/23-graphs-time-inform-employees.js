69317bfeecbf2d9850e3f78aa701332a29caf991
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-01 12:31:55 -0500

    Update and rename 23-graph-time-needed-inform-employees.js to 23-graphs-time-inform-employees.js

---
+++ ztm-leetcode/23-graphs-time-inform-employees.js
@@ -0,0 +1,8 @@
+step 1: verify the constraints
+step 2: write out some test cases
+step 3: figure out a solution without code
+step 4: write out our solution in code
+step 5: double check for errors
+step 6: test our code with our test cases
+step 7: space & time complexity
+step 8: can we optimize our solution?
