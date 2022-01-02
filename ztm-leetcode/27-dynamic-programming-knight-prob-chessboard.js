be9ea0074d362a5e3ce8c53b88eb47ee726c9b6a
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-02 14:18:38 -0500

    Update and rename 27-dynamic-programming.js to 27-dynamic-programming-knight-prob-chessboard.js

---
+++ ztm-leetcode/27-dynamic-programming-knight-prob-chessboard.js
@@ -0,0 +1,9 @@
+//Knight Probability In Chessboard
+
+On an n x n chessboard, a knight starts at the cell (row, column) and attempts to make exactly k moves. The rows and columns are 0-indexed, so the top-left cell is (0, 0), and the bottom-right cell is (n - 1, n - 1).
+
+Input: n = 3, k = 2, row = 0, column = 0
+Output: 0.06250
+
+Input: n = 1, k = 0, row = 0, column = 0
+Output: 1.00000
