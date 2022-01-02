29917baacf7ddd3dd55053712125187810c090ba
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-01 21:29:24 -0500

    Update 30-tries-implement-prefix-trie.js

--- ztm-leetcode/30-tries-implement-prefix-trie.js
+++ ztm-leetcode/30-tries-implement-prefix-trie.js
@@ -16,48 +16,50 @@ interface Trie{
 }
 
 //Implement Prefix Trie
-class TrieNode {
+//Time: O(L)  - L is the lenth of the word
+//Space: O(N) O(L)
+class TrieNode {    //each trie node
   constructor() {
-    this.end = false;
-    this.keys = {};
+    this.end = false;   //whether the node is the end
+    this.keys = {};   //keys we can extend from this node
   }
 }
 
 class Trie {
   constructor() {
-    this.root = new TrieNode();
+    this.root = new TrieNode();   //a new TrieNode
   }
 
   insert(word, node = this.root) {
-    if(word.length === 0) {
-      node.end = true;
-      return;
-    } else if (!node.keys[word[0]]) {
-      node.keys[word[0]] = new TrieNode();
-      this.insert(word.substring(1), node.keys[word[0]]);
+    if(word.length === 0) { //when the word length is 0
+      node.end = true;  //it would be the end of the word
+      return;   //done with this iteration
+    } else if (!node.keys[word[0]]) {   //the first character does not exist in the keys{}
+      node.keys[word[0]] = new TrieNode();    //create a new node
+      this.insert(word.substring(1), node.keys[word[0]]); //cut the first character, recursive call insert and pass down the rest of the word
     } else {
-      this.insert(word.substring(1), node.keys[word[0]]);
+      this.insert(word.substring(1), node.keys[word[0]]); //cut the first character, recursive call insert and pass down the rest of the word
     }
   }
 
-  search(word, node = this.root) {
-    if(word.length === 0 && node.end) {
+  search(word, node = this.root) {  //receive the word and the root node
+    if(word.length === 0 && node.end) { //from the word itself we finish checking everything, return true
       return true;
-    } else if(word.length === 0) {
+    } else if(word.length === 0) {  //if failed above and the length of the word is 0, return false
       return false;
-    } else if(!node.keys[word[0]]) {
+    } else if(!node.keys[word[0]]) {  //if the character does not exist, return false
       return false;
-    } else {
+    } else {  //after all above, recursive call again until (word.length === 0 && node.end) which will be the true case
       return this.search(word.substring(1), node.keys[word[0]]);
     }
   }
 
   startsWith(prefix, node = this.root) {
-    if(prefix.length === 0) {
+    if(prefix.length === 0) {   //to the length of prefix to 0, then return true
       return true;
-    } else if(!node.keys.hasOwnProperty(prefix[0])) {
+    } else if(!node.keys.hasOwnProperty(prefix[0])) { //no such key at the current node
       return false
-    } else {
+    } else {    //continue the recusive traversal call
       return this.startsWith(prefix.substring(1), node.keys[prefix[0]]);
     }
   }
