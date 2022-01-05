527a098357cbb31f08737a55fd858d958f71248f
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-05 00:24:20 -0500

    Update 013-roman-integer.js

--- leetcode/013-roman-integer.js
+++ leetcode/013-roman-integer.js
@@ -1,1 +1,26 @@
-
+var romanToInt = function(s) {
+    const map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    if(s.length < 1) return 0;
+    let array = [...s];
+    let value = 0;
+    for (let i = 0; i < array.length; i++) {
+        let current = map[array[i]];
+        let next = map[array[i + 1]];
+        if (next > current) {
+            value += next - current;
+            i++;
+        } else {
+            value += current;
+        }
+    }
+    return value;
+};
+ 
