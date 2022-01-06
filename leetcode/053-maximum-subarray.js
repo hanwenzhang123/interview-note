a35527ba7f7e3ae30b31ad5e79b5b3ce6f177620
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-06 17:08:25 -0500

    Update 053-maximum-subarray.js

--- leetcode/053-maximum-subarray.js
+++ leetcode/053-maximum-subarray.js
@@ -1,1 +1,14 @@
 
+var maxSubArray = function(nums) {
+  let sum = 0;
+  let max = nums[0];
+  for (let i = 0; i < nums.length; i++) {
+  	sum += nums[i]
+	max = Math.max(max, sum); // Get the max of max and sum
+	if (sum < 0) {           // If the sum is less than 0, reset the sum to 0
+		sum = 0;
+	}
+  }
+  return max;
+  
+};
