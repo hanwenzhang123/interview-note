82aa40c05b319aab1a3789db5bc18599864160db
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-06 16:33:47 -0500

    Update 027-remove-element.js

--- leetcode/027-remove-element.js
+++ leetcode/027-remove-element.js
@@ -1,1 +1,11 @@
+var removeElement = function(nums, val) {
+    if(nums.length === 0) return 0;
+    for (let i=0; i<nums.length; i++){
+        if(nums[i] === val){
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return nums.length;
+};
 
