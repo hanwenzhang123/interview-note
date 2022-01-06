df15e0f778f889cd805b16e23d0173b9a38ba86a
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-06 13:13:30 -0500

    Update 021-merge-two-sorted-lists.js

--- leetcode/021-merge-two-sorted-lists.js
+++ leetcode/021-merge-two-sorted-lists.js
@@ -1,16 +1,16 @@
 var mergeTwoLists = function(list1, list2) {
     let result = new ListNode();
-    let current = result;
+    let current = result;   //a dummy node to point to the next value
 
-    while(list1 && list2){  //when both lists same length
+    while(list1 && list2){  //when both lists have value
         if(list1.val<list2.val){
-            current.next = list1;
-            list1 = list1.next; //push the value to the current
+            current.next = list1;  //let next points to list1 value
+            list1 = list1.next; //push the next value to the list1
         } else {
-            current.next = list2;
-            list2 = list2.next; //push the value to the current
+            current.next = list2; //let next points to list2 value
+            list2 = list2.next; //push the next value to the list2
         }
-        current = current.next;
+        current = current.next; //change current to current.next (either list1 or list2) based on the comparison to avoid overwritten
     }
     current.next = list1 || list2;  //remaining listnode
 
