f90e864e018469011d9a4dfd37771c27f3ee1dae
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-06 16:58:38 -0500

    Update 028-implement-strStr().js

--- leetcode/028-implement-strStr().js
+++ leetcode/028-implement-strStr().js
@@ -1,4 +1,13 @@
 var strStr = function(haystack, needle) {
+    if(needle.length === 0 ) return 0
+    if(!haystack.includes(needle)) return -1
+    if(needle && haystack.includes(needle)){
+        var arr = haystack.split(needle)
+        return arr[0].length
+    }
+};
+
+var strStr = function(haystack, needle) {
   if(haystack.length === 0 || needle.length === 0) return 0;
   for(let i=0; i<=haystack.length - needle.length; i++){
       if(haystack[i] == needle[0]){
@@ -17,11 +26,3 @@ var strStr = function(haystack, needle) {
   return -1;
 };
 
-var strStr = function(haystack, needle) {
-    if(needle.length === 0 ) return 0
-    if(!haystack.includes(needle)) return -1
-    if(needle && haystack.includes(needle)){
-        var arr = haystack.split(needle)
-        return arr[0].length
-    }
-};
