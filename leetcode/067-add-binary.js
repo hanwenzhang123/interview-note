9459665547071c7aa43dbefba913b4ae1b1564d5
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-10 13:07:31 -0500

    Update 067-add-binary.js

--- leetcode/067-add-binary.js
+++ leetcode/067-add-binary.js
@@ -1,1 +1,34 @@
+let addBinaryManual = (A, B) => {
+  
+}
 
+var addBinary = function (a, b) {
+    let op = '',
+        carry = 0;
+    let bigger = a.length > b.length ? a : b;
+    let smaller = a.length > b.length ? b : a;
+    for (let i = bigger.length - 1, j = smaller.length - 1; i >= 0; i--, j--) {
+        let sum = parseInt(bigger[i]) + parseInt(smaller[j] || 0) + carry;
+        if (sum === 0) {
+            carry = 0;
+            op = '0' + op;
+        } else if (sum === 1) {
+            carry = 0;
+            op = '1' + op;
+        } else if (sum === 2) {
+            carry = 1;
+            op = '0' + op;
+        } else {
+            carry = 1;
+            op = '1' + op;
+        }
+    }
+    op = carry ? carry + op : op;
+    return op;
+};
+
+var addBinary = function(a, b) {
+    return (parseInt(a, 2) + parseInt(b, 2)).toString(2);  
+    //toString in base 2 
+    //parseInt convert string output number in decimal
+};
