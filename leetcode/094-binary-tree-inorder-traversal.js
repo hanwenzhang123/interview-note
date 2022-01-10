94350d9b1ff3a2962dc8356fb5c77857c3ba0f30
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-10 16:16:47 -0500

    Update 094-binary-tree-inorder-traversal.js

--- leetcode/094-binary-tree-inorder-traversal.js
+++ leetcode/094-binary-tree-inorder-traversal.js
@@ -1,1 +1,41 @@
+//Solution #1
+var inorderTraversal = function(root) {
+    let res = [];
+    let stack = [];
+    let curr = root;
+    while (curr || stack.length !== 0) {
+        while (curr) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+        curr = stack.pop();
+        res.push(curr.val);
+        curr = curr.right;
+    }
+    return res;
+};
 
+//Solution #2
+var inorderTraversal = function(root) {
+   return visitNode(root, []);
+};
+
+var visitNode = function(root, arr) {
+    if (!root) {
+        return [];
+    }
+    
+    if (root.left){
+        visitNode(root.left, arr);
+    }
+    
+    if (root){
+        arr.push(root.val);
+    }
+    
+    if (root.right){
+        visitNode(root.right, arr);
+    }   
+    
+    return arr;
+}
