b09636608fa1ab67ba865dbd346928ae6439fb87
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-10 16:52:33 -0500

    Update 100-same-tree.js

--- leetcode/100-same-tree.js
+++ leetcode/100-same-tree.js
@@ -1,1 +1,13 @@
+// null?
+// different?
+// isSame(right, right) && isSame(left, left)
 
+var isSameTree = function(p, q) {
+    if (p === null || q === null) { //when one is null, another is not, return false
+        return p === q;     //if both are the same, return true;
+    }
+    if (p.val !== q.val){   //when both p or q value is different 
+        return false;
+    }
+    return isSameTree(p.right, p.right) && isSameTree(p.left, p.left);  //recursion call, check right and left
+};
