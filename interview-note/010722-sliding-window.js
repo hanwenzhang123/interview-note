a3139482617703a42cb66d0a8186460d0063da79
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-09 22:42:17 -0500

    Update 010722-sliding-window.js

--- interview-note/010722-sliding-window.js
+++ interview-note/010722-sliding-window.js
@@ -1,6 +1,20 @@
 //010722 - BeaconFire
 //Sliding Window Technique
-
+function maxSubArray(nums, size){
+  if(size<0 || size>arr.length) return null;
+  let currSum = 0;
+  let maxSumSeen = -Infinity;
+  for(let i = 0; i < nums.length; i++){
+    currSum += nums[i];
+    if(i >= size-1){
+      maxSumSeen = Math.max(currSum, maxSumSeen);
+      currSum -= nums[i-(size-1)];
+    }
+  }
+  return maxSumSeen;
+}
+const arr = [1,2,3,4,5,4,8,6,2];
+console.log(maxSubArray(arr,3));
 
 //Question: pick 3 element either from beginning or end, and return the max adding num from the array.
 const calculation = (arr, k) => {
