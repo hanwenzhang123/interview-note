077168c92b32b565480d5a0871fad44998cdcd89
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-12 12:19:29 -0500

    Update 010722-sliding-window.js

--- interview-note/010722-sliding-window.js
+++ interview-note/010722-sliding-window.js
@@ -78,3 +78,60 @@ const isAnagram = (firstWord, secondWord) => {
 
 console.log(isAnagram(firstWord, secondWord));
  
+//01/20/2022 - Analytic Partners
+let vacStatus = [
+	{"st": "Connecticut", "date": "12/06/2021", "vac": 3031723},
+	{"st": "New Jersey", "date": "01/11/2022", "vac": 7564407},
+	{"st": "New York", "date": "01/10/2022", "vac": 16626152},
+	{"st": "Pennsylvania", "date": "01/11/2022", "vac": 10218537},
+	{"st": "Rhode Island", "date": "01/11/2022", "vac": 961320},
+	{"st": "Vermont", "date": "01/11/2022", "vac": 564218},
+];
+
+let census2020 = [
+    {"st": "Connecticut", "ppl": 3605944},
+    {"st": "Maine", "ppl": 1362359},
+    {"st": "Massachusetts", "ppl": 7029917},
+    {"st": "New Hampshire", "ppl": 1377529},
+    {"st": "Rhode Island", "ppl": 1097379},
+    {"st": "Vermont", "ppl": 643077},
+];
+
+// vaccination rate = vac / ppl:
+Conn r = 3031723 / 3605944 = 0.7
+Rhode Island r = 961320 / 1097379 = 0.9
+
+Jan 12, 2022 -> 	1642022959
+Jan 11, 2022 -> 	1641936559
+let convertDateToTs = function(date) {
+	// date: "01/12/2022"
+	let timeStamp = new Date() ... getTime()
+	return timeStamp;
+}
+
+//Solution
+let vacRate = [];
+for (i = 0; i < vacStatus.length; i++ ){
+	let state = vacStatus[i]["st"]
+  let vacPpl = vacStatus[i]["vac"]
+  let tempData = {"st": state, "r": null};
+  for (let each of census2020) {
+  	if(each["st"] == state){
+    	let pplInfo = each.ppl;
+      tempData = {"st": state, "r": vacPpl / pplInfo};
+      vacRate.push(tempData)
+  	}
+  }
+  vacRate.push(tempData)
+}
+console.log(vacRate)
+
+// Your result should look like:
+let vacRate = [
+	{"st": "Connecticut", "r": 3031723 / 3605944},
+	{"st": "New Jersey", "r": null},
+	{"st": "New York", "r": null},
+	{"st": "Pennsylvania", "r": null},
+	{"st": "Rhode Island", "r": 961320 / 1097379},
+	{"st": "Vermont", "r": 564218 / 643077},
+];
