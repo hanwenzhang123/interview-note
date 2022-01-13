efc805c7e2eaab18b78ef3086004faf1b7ea1fb0
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-13 12:46:51 -0500

    Create 013122-best-marketing.js

---
+++ interview-note/013122-best-marketing.js
@@ -0,0 +1,28 @@
+1/12/2022 - Applex
+ 
+Experience
+ 
+What project you develop with React
+ 
+React state
+ 
+The flow to develop the feedback feature page (comments + 5 star ratings)
+ 
+Five components we need to support this
+Textarea for comments, input field for ratings, edit button, submit button
+Reusable components for this features
+Button, input and textarea
+ 
+Vue experience
+ 
+Tech can improve performance
+lighthouse
+Responsive design
+ 
+Cross-browser ability
+Media query for style
+ 
+ 
+ 
+Phone number(number type + length)
+80 length - 120
