12315f95844e0e09a6569fb07a8fcc7c8645b2ed
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-22 22:12:30 -0500

    Update 844-backspace-string-compare.js

--- leetcode/844-backspace-string-compare.js
+++ leetcode/844-backspace-string-compare.js
@@ -1,1 +1,17 @@
+//Solution #1
+var backspaceCompare = function(s, t) {
+    const str = (str) => {
+        const stack = [];
+        for(let c of str) {
+            if(c === "#"){
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.join("")
+    }
+    
+    return str(s) === str(t);
+};
 
