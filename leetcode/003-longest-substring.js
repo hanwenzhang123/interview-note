98e99402baca9bd02ea29e09648ab5e27a223798
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-22 21:04:22 -0500

    Update 003-longest-substring.js

--- leetcode/003-longest-substring.js
+++ leetcode/003-longest-substring.js
@@ -1,3 +1,4 @@
+//Solution 1
 const lengthOfLongestSubstring = function(s) {
     if(s.length <= 1) return s.length;
     
@@ -13,11 +14,59 @@ const lengthOfLongestSubstring = function(s) {
           currentLength++; 
           seenChars[currentChar] = true; 
           longest = Math.max(longest, currentLength); 
-        } else {    //if it exist in the seenChars
+        } else { 
           break; 
         }
       }
     }
     
     return longest;
+};
+
+//Solution 2
+var lengthOfLongestSubstring = function(s) {
+    if(s.length < 2) return s.length;
+    let longest = 0;
+    let substring = "";
+    
+    for(let character of s) {
+        let index = substring.indexOf(character);
+        if (index == -1) {
+            substring+=character
+            longest = Math.max(longest, substring.length)
+        } else {
+            substring = substring.substring(index+1)+character
+//             let tempArr = substring.split("")
+//             tempArr.splice(0, index+1)
+//             substring = tempArr.join("")
+//             substring+=character
+        }
+
+    }
+
+    return longest;
+    
+};
+
+//Solution #3
+var lengthOfLongestSubstring = function(s) {
+    if(s.length < 2) return s.length;
+    let set = new Set();
+    let longest = 0;
+    let left = 0;
+    let right = 0;
+    
+    while(right < s.length){
+        if (!set.has(s.charAt(right))) {
+            set.add(s.charAt(right));
+            longest = Math.max(longest, set.size);
+            right++;
+        } else {
+            set.delete(s.charAt(left));
+            left++;
+        }
+    }
+
+    return longest;
+    
 };
