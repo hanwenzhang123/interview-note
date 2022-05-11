dcb5b3f9b2d48a87b44c33c9bf9f704272449916
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-05-11 12:15:47 -0400

    Create 053122-best-marketing.js

---
+++ best-marketing/053122-best-marketing.js
@@ -0,0 +1,60 @@
+5/6 Frank vendor: 
+ 
+Intro
+React version
+JS vs TS
+Es 6 engine??
+Closure
+Jquery
+React feature
+Class component vs functional component
+Component 
+State and props
+Redux
+What is action
+Advantage of Reusable component
+AWS?
+Test Jest and Enzyme
+
+
+5/5 Amber infoway call(Melvin immanuel)
+1.intro
+2.react advantages
+3.Hooks
+ 
+ 
+5/4 Cara client: Apple(Rachael) 面试官： YUHUA HE
+1.intro/detailed components used in project
+2.Form validation
+3. Accessibility(detailed ways to implement accessibility)
+	ex:-tabIndex= “0” 按tab就会转到下一个input，不可以是negtive number
+   -<input aria-label={labelText}></input>
+	    aria-label 给此element一个label，可以被voice loader读出来
+	   --If the tile is an icon, not words, just give tile=””, so that they can know the meaning of icon
+4. Coding: 
+a.explain CSS selector given			
+b.Fix header and footer with scrollable content
+C. isPalidrome
+ 
+const isPalidrome = (str) => {
+ let left = 0,
+   right = str.length - 1;
+ while (left < right) {
+ 	while(left<str.legnth && str[left]===" ") left++
+ 	while(right>0 && str[right]===" ") right-–
+if(left < right) break
+ 	
+   if (str[left] !== str[right]) {
+     return false;
+   }
+   left++;
+   right--;
+ }
+ return true;
+};
+
+6.Giving a task(product/type/inventory/price), assume API and data are ready to use, ask PM questions
+
+
+5/4 Jenna client: visa 面试官：Venkataraman Sathyanarayanan
+Coding：
